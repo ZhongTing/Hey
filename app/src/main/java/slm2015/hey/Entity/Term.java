abf69445f93980ed13e07bc35a2ed315bb283032
kevinho <kwite2002@gmail.com>
2015-04-10 22:02:27 +0800

    add Term in entity file

---
+++ app/src/main/java/slm2015/hey/Entity/Term.java
@@ -0,0 +1,27 @@
+package slm2015.hey.entity;
+
+public class Term {
+    private String term;
+    private boolean isSelected;
+
+    public Term(String term){
+        this.term = term;
+        this.isSelected = false;
+    }
+
+    public String getTerm(){
+        return this.term;
+    }
+
+    public void setTerm(String term){
+        this.term = term;
+    }
+
+    public boolean isSelected(){
+        return this.isSelected;
+    }
+
+    public void setIsSelected(boolean isSelected){
+        this.isSelected = isSelected;
+    }
+}
