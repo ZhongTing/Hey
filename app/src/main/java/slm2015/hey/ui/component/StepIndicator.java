ee199fec4427085392fb6f06382d64aa537a225f
= <gary62107@gmail.com.tw>
2015-04-22 05:29:01 +0800

    StepIndicator 有它該有的樣子了XD 支援 app:text  app:active兩個xml屬性

--- app/src/main/java/slm2015/hey/ui/component/StepIndicator.java
+++ app/src/main/java/slm2015/hey/ui/component/StepIndicator.java
@@ -2,14 +2,19 @@ package slm2015.hey.ui.component;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import slm2015.hey.R;
 
 public class StepIndicator extends FrameLayout{
     private TextView indicateTextView;
+    private ImageView indicateHeadImageView;
+    private Boolean active;
+    private String indicateText;
 
     public StepIndicator(Context context) {
         super(context);
@@ -35,12 +40,26 @@ public class StepIndicator extends FrameLayout{
     private void loadAttribute(AttributeSet attrs, int defStyle) {
         final TypedArray a = getContext().obtainStyledAttributes(
                 attrs, R.styleable.StepIndicator, defStyle, 0);
-
-        this.indicateTextView.setText(a.getString(R.styleable.StepIndicator_text));
+        this.setActive(a.getBoolean(R.styleable.StepIndicator_active, false));
+        this.setIndicateText(a.getString(R.styleable.StepIndicator_text));
         a.recycle();
     }
 
     private void findview() {
         this.indicateTextView = (TextView) findViewById(R.id.step_indicator_text);
+        this.indicateHeadImageView = (ImageView) findViewById(R.id.step_indicator_head);
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+        int color = active ? R.color.blue_green : R.color.peach_red;
+        int source = active ? R.drawable.arrow_head : R.drawable.arrow_head_unactive;
+        this.indicateTextView.setBackgroundResource(color);
+        this.indicateHeadImageView.setImageResource(source);
+    }
+
+    public void setIndicateText(String indicateText) {
+        this.indicateText = indicateText;
+        this.indicateTextView.setText(indicateText);
     }
 }
