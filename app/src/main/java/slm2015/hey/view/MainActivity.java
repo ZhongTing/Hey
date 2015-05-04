794f39c9e5f769e82a55af0683867109762974a8
Keming <believe.75467@gmail.com>
2015-05-05 00:06:51 +0800

    刪除無用的code (舊的MainActivity)

--- app/src/main/java/slm2015/hey/view/MainActivity.java
+++ app/src/main/java/slm2015/hey/view/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hey_main);
+        setContentView(R.layout.activity_main);
 
         PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
