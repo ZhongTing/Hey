5068509b3314041c4eb86895c71e73dba95f6094
Keming <believe.75467@gmail.com>
2015-05-06 18:19:23 +0800

    加入Term Handler & Loader

---
+++ app/src/main/java/slm2015/hey/core/term/TermHandler.java
@@ -0,0 +1,11 @@
+package slm2015.hey.core.term;
+
+import android.content.Context;
+
+import slm2015.hey.core.BaseAPIHandler;
+
+public class TermHandler extends BaseAPIHandler {
+    public TermHandler(Context context) {
+        super(context);
+    }
+}
---
+++ app/src/main/java/slm2015/hey/core/term/TermLoader.java
@@ -0,0 +1,9 @@
+package slm2015.hey.core.term;
+
+import slm2015.hey.core.Subject;
+
+public class TermLoader extends Subject{
+    public TermLoader(){
+
+    }
+}
