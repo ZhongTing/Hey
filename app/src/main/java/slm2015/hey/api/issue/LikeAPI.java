7c24ac2708373e24318621871eda3f91a1d12ddc
ZhongTing <gary62107@gmail.com>
2015-06-21 01:15:13 +0800

    like/regret api （history簡單介接測試有無成功）

---
+++ app/src/main/java/slm2015/hey/api/issue/LikeAPI.java
@@ -0,0 +1,16 @@
+package slm2015.hey.api.issue;
+
+import slm2015.hey.api.APIManager;
+import slm2015.hey.api.PostBase;
+
+public class LikeAPI extends PostBase {
+    public LikeAPI(int issueId) {
+        super(APIManager.HEY_SERVER_BASE_URL, "/api/issue/like");
+
+        this.setHeader("Authorization", APIManager.getInstance().getAccessToken());
+
+        this.setParam("issueId", issueId + "");
+
+        this.TAG = "LikeAPI";
+    }
+}
